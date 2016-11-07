4bf330cf7f422688e3d594123f1112259ee06eb4
Jim Padilla and Margie Nevarez <margie@>
2016-11-07 15:04:18 -0800

    prototype to check boardsection

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,64 @@ function Board(){
   this.gridCoords1 = [0, 1, 2, 3, 4, 5, 6, 7],
   this.gridCoords2 = [0, 1, 2, 3, 4, 5, 6, 7]
 }
+
 var indexOfgridCoords1;
 var indexOfgridCoords2;
+var theBoard;
+var cardSpot;
+var boardSection;
 
+// Board.prototype.cardState = function(currentCard) {
+//
+//   for (i = 0; i < theBoard.gridCoords.length; i++) {
+//
+//   if (theBoard.gridCoords[i] === "clicked") {
+//     // this code executes when it find a clicked grid
+//       console.log(theBoard.gridCoords.indexOf(theBoard.gridCoords[i]));
+//       theBoard.gridCoords[i] = i;
+//     }
+//
+//   }
+// }
 
- var matchFinder = function() {
-   for (i = 0; i < boardGame.gridCoords1.length; i++) {
-
-     if (boardGame.gridCoords1[i] === "clicked") {
-        indexOfgridCoords1 = boardGame.gridCoords1.indexOf(boardGame.gridCoords1[i]);
-     }
-   }
-
-    for (i = 0; i < boardGame.gridCoords2.length; i++) {
-
-     if (boardGame.gridCoords2[i] === "clicked") {
-        indexOfgridCoords2 = boardGame.gridCoords2.indexOf(boardGame.gridCoords2[i]);
-     }
-    }
-
-   console.log(indexOfgridCoords1 + " " + indexOfgridCoords2);
-
- }
-
-Board.prototype.cardState = function(currentCard) {
-
-  for (i = 0; i < theBoard.gridCoords.length; i++) {
-
-  if (theBoard.gridCoords[i] === "clicked") {
-    // this code executes when it find a clicked grid
-      console.log(theBoard.gridCoords.indexOf(theBoard.gridCoords[i]));
-      theBoard.gridCoords[i] = i;
-    }
-
+Board.prototype.boardCheck = function(boardsection, currentCard) {
+  if (boardsection === 2) {
+    theBoard.gridCoords1[currentCard] = "clicked";
+    return "board 2"
+  } else if (boardsection === 3) {
+    theBoard.gridCoords2[currentCard] = "clicked";
+    return "board 3"
   }
 }
 
 Board.prototype.cardStateChanger = function(currentCard) {
-  theBoard.gridCoords[currentCard] = "clicked";
+
+  var boardsection = theBoard.boardCheck(boardSection, currentCard);
+
+  if (boardsection === "board 2") {
+    theBoard.gridCoords1[currentCard] = "clicked";
+  } else if (boardsection === "board 3") {
+    theBoard.gridCoords2[currentCard] = "clicked";
+  }
+
 }
 
 Board.prototype.matchFinder = function() {
+
+  for (i = 0; i < theBoard.gridCoords1.length; i++) {
+
+    if (theBoard.gridCoords1[i] === "clicked") {
+       indexOfgridCoords1 = theBoard.gridCoords1.indexOf(theBoard.gridCoords1[i]);
+    }
+  }
+
+   for (i = 0; i < theBoard.gridCoords2.length; i++) {
+
+    if (theBoard.gridCoords2[i] === "clicked") {
+       indexOfgridCoords2 = theBoard.gridCoords2.indexOf(theBoard.gridCoords2[i]);
+    }
+   }
+
     if (indexOfgridCoords1 === indexOfgridCoords2) {
      var showImage = function(cardSpot) {
         $("." + cardSpot).html("");
@@ -66,8 +83,6 @@ Board.prototype.matchFinder = function() {
 
 
 
-var theBoard;
-var cardSpot;
 
 
 $(document).ready(function(){
@@ -79,25 +94,13 @@ $(document).ready(function(){
   });
 
   $(".grid").click(function() {
+    debugger;
     cardSpot = parseInt($(this).attr("class")); // grabs id and converts to number
+    boardSection = parseInt($(this).parent().attr("class"));
+
     theBoard.cardStateChanger(cardSpot);
 
-
-    var showImage = function(cardSpot) {
-      $("." + cardSpot).html("");
-      $("." + cardSpot).html("<h1><img class=' img-responsive' src='img/" + cardSpot + ".jpg'></h1>"); // Inserts image in proper grid square by ID
-    };
-
-  showImage(cardSpot);
-
-  var cardBack = function(cardSpot) {
-    $("." + cardSpot).html("");
-    $("." + cardSpot).html("<h1><img src='img/question.jpg'></h1>");//inserts question image after x seconds
-  }
-
-  setTimeout(function(){
-    cardBack(cardSpot);
-   }, 500);
+    theBoard.matchFinder();
 
 
 
