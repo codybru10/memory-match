9ed62070cb7d56061a32ec8e698f76093808ea13
Cody Brubaker and Taylor Loftis-Kim <cody@>
2016-11-08 11:47:34 -0800

    add javascript to make matches and mismatched

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ function Board(){
 var theBoard;
 var cardSpot;
 var boardSection;
+var indexOfgridCoords1;
+var indexOfgridCoords2;
 
 // Board.prototype.cardState = function(currentCard) {
 //
@@ -43,7 +45,7 @@ Board.prototype.cardStateChanger = function(currentCard, boardsection) {
 
 }
 
-Board.prototype.matchFinder = function(indexOfgridCoords1, indexOfgridCoords2) {
+Board.prototype.matchFinder = function() {
 
   for (i = 0; i < theBoard.gridCoords1.length; i++) {
 
@@ -61,25 +63,64 @@ Board.prototype.matchFinder = function(indexOfgridCoords1, indexOfgridCoords2) {
     }
    }
 
-    if (indexOfgridCoords1 === indexOfgridCoords2) {
+   debugger;
+   if (indexOfgridCoords1 === indexOfgridCoords2) {
+     theBoard.gridCoords1[indexOfgridCoords1] = "matched";
+     theBoard.gridCoords2[indexOfgridCoords2] = "matched";
+     showImage2(indexOfgridCoords1,indexOfgridCoords2);
+     indexOfgridCoords1 = undefined;
+     indexOfgridCoords2 = undefined;
+   } else if (indexOfgridCoords1 !== indexOfgridCoords2 && indexOfgridCoords2 !== undefined && indexOfgridCoords1 !== undefined) {
+      pictureTimer();
+   } else {
+     showImage(cardSpot);
+   }
 
-      showImage(cardSpot);
-    } else {
-      setTimeout(function(){
-   cardBack(cardSpot);
-  }, 500);
-  }
+
+
+    //if (indexOfgridCoords1 === indexOfgridCoords2) {
+      //showImage2(indexOfgridCoords1,indexOfgridCoords2);
+  //  } else if (indexOfgridCoords1 !== indexOfgridCoords2 && indexOfgridCoords2 !== undefined && indexOfgridCoords1 !== undefined) {
+  //    cardBack2(indexOfgridCoords1,indexOfgridCoords2);
+    //  for (i = 0; i)
+//  }
 
 }
 
+var pictureTimer = function() {
+debugger;
+  showImage(indexOfgridCoords2);
+  setTimeout(function() {
+  cardBack2(indexOfgridCoords1, indexOfgridCoords2); }, 3000);
+}
+
 var showImage = function(cardSpot) {
+debugger;
    $("." + boardSection).find("." + cardSpot).html("");
    $("." + boardSection).find("." + cardSpot).html("<h1><img class='img-responsive' src='img/" + cardSpot + ".jpg'></h1>"); // Inserts image in proper grid square by ID
+ };
+
+var showImage2 = function(indexOfgridCoords1,indexOfgridCoords2) {
+   $("." + boardSection).find("." + indexOfgridCoords1).html("");
+   $("." + boardSection).find("." + indexOfgridCoords1).html("<h1><img class='img-responsive' src='img/" + cardSpot + ".jpg'></h1>"); // Inserts image in proper grid square by ID
+   $("." + boardSection).find("." + indexOfgridCoords2).html("");
+   $("." + boardSection).find("." + indexOfgridCoords2).html("<h1><img class='img-responsive' src='img/" + cardSpot + ".jpg'></h1>"); // Inserts image in proper grid square by ID
  };
 
  var cardBack = function(cardSpot) {
   $("." + boardSection).find("." + cardSpot).html("");
   $("." + boardSection).find("." + cardSpot).html("<h1><img class=class='img-responsive' src='img/question.jpg'></h1>"); //inserts question image
+
+}
+
+ var cardBack2 = function(cardSpot1,cardSpot2) {
+  debugger;
+  $(".98").find("." + cardSpot1).html("");
+  $(".98").find("." + cardSpot1).html("<h1><img class=class='img-responsive' src='img/question.jpg'></h1>"); //inserts question image
+  $(".99").find("." + cardSpot2).html("");
+  $(".99").find("." + cardSpot2).html("<h1><img class=class='img-responsive' src='img/question.jpg'></h1>"); //inserts question image
+  indexOfgridCoords1 = undefined;
+  indexOfgridCoords2 = undefined;
 
 }
 
@@ -96,7 +137,7 @@ $(document).ready(function(){
   $(".grid").click(function() {
     cardSpot = parseInt($(this).attr("class")); // grabs id and converts to number
     boardSection = parseInt($(this).parent().attr("class"));
-    showImage(cardSpot);
+    //showImage(cardSpot);
     theBoard.cardStateChanger(cardSpot, boardSection);
 
     theBoard.matchFinder();
